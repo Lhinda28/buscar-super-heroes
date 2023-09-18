32598187d4c5506b49cb45ce46c79f0462d9debe
Linda Loza <lindaloza8@gmail.com>
2023-09-18 12:18:43 -0600

    creamos componentes, hicimos una lista de heroes, pusimos un input con buscador

---
+++ super_heroes_app/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Ironman' },
+  { id: 12, name: 'Capitan America' },
+  { id: 13, name: 'Thor' },
+  { id: 14, name: 'Rocket' },
+  { id: 15, name: 'Magneto' },
+  { id: 16, name: 'Groot' },
+  { id: 17, name: 'Hulk' },
+  { id: 18, name: 'Wanda' },
+  { id: 19, name: 'Vision' },
+  { id: 20, name: 'Black Widow' }
+];
